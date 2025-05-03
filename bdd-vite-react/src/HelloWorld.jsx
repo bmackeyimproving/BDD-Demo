e1d0c38a460fc80994f20a55c4ce4455ce3b454d
bmackeyimproving <brian.mackey@improving.com>
2025-05-02 23:33:24 -0500

    Cleanup

--- bdd-vite-react/src/HelloWorld.jsx
+++ bdd-vite-react/src/HelloWorld.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-export default function HelloWorld({ text = "Hello World" }) {
+export default function HelloWorld({ text: defaultText = "Hello World" }) {
   const [count, setCount] = React.useState(0);
+  const [text, setText] = React.useState(defaultText);
   return (
     <div>
       <div>{text}</div>
       <button onClick={() => setCount((c) => c + 1)}>
         Clicked {count} times
       </button>
-      <button onClick={() => setCount(0)}>
+      <button onClick={() => {setCount(0); setText(defaultText); }}>
         Reset
       </button>
     </div>
