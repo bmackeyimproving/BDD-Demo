70d3ea45a5149da44c491d60f0d39f96c56991a8
bmackeyimproving <brian.mackey@improving.com>
2025-05-03 09:11:08 -0500

    Add Jest

---
+++ bdd-vite-react/src/Adder.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function add(a, b) {
+  return a + b;
+}
+
+export default function Adder() {
+  return (
+    <div>
+      <h2>Adder Component</h2>
+    </div>
+  );
+}
