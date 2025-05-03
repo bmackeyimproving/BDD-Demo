43a72fe50f3eed9743748d0ff0c924b844da56e1
bmackeyimproving <brian.mackey@improving.com>
2025-05-03 09:24:18 -0500

    cleanup

--- bdd-vite-react/src/App.jsx
+++ bdd-vite-react/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import HelloWorld from './HelloWorld'
 import Greeting from './Greeting';
-import Farewell from './Farewell';
 import Adder from './Adder';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>BDD</h1>
