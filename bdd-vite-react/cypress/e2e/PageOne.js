7539fcafabc1c42c57083cc7ea5319df150c6503
bmackeyimproving <brian.mackey@improving.com>
2025-05-04 16:21:57 -0500

    Update PageOne.js

--- bdd-vite-react/cypress/e2e/PageOne.js
+++ bdd-vite-react/cypress/e2e/PageOne.js
@@ -1,5 +1,13 @@
 import { Given, Then } from '@badeball/cypress-cucumber-preprocessor';
 
+beforeEach(() => {
+  cy.log('fake login')
+});
+
+afterEach(() => {
+  cy.log('fake logout')
+});
+
 Given('I open the Page One page', () => {
   cy.visit('/'); // Adjust the URL to match the route for Page One
 });
