43c8e7daa127b2b325c6db3e3d047e583f111c5a
bmackeyimproving <brian.mackey@improving.com>
2025-05-04 18:33:19 -0500

    Add lint.  Remove obsolete files.

--- bdd-vite-react/eslint.config.js
+++ bdd-vite-react/eslint.config.js
@@ -1,22 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsEslintParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  { ignores: ['dist', 'node_modules', 'cypress'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      '@typescript-eslint': tsEslintPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    parser: tsEslintParser,
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
@@ -25,4 +31,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+];
--- bdd-vite-react/src/bdd/features/greeting.feature
+++
@@ -1,9 +0,0 @@
-Feature: Greeting Component
-  As a user
-  I want to see a greeting message
-  So that I feel welcomed
-
-  Scenario: Display greeting message
-    Given I have a name "Alice"
-    When I render the Greeting component
-    Then I should see "Hello, Alice!" message twice because it is in 2 components
