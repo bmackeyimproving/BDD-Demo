4b7b0d43c82a1d27004ef9f0f51ac1b3f00eaaef
bmackeyimproving <brian.mackey@improving.com>
2025-05-03 21:44:21 -0500

    Add partial data grid
    Holding off on implementation because it breaks the BDD testing framework

--- bdd-vite-react/src/pages/PageTwo.jsx
+++ bdd-vite-react/src/pages/PageTwo.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useSharedService } from '../services/sharedService';
+import { getMockData } from '../services/mockDataService';
 
 function PageTwo() {
   const { getMessage } = useSharedService();
+  const rows = getMockData();
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'value', headerName: 'Value', width: 150 },
+  ];
+
   return (
-    <div>
-      <h1>Page Two</h1>
-      <p>{getMessage()}</p>
-      <div data-testid="get-data">Get Data in ways that are not fragile</div>
-    </div>
+      <div>
+        <h1>Page Two</h1>
+        <p>{getMessage()}</p>
+        <div data-testid="get-data">Get Data in ways that are not fragile</div>
+        <div style={{ height: 400, width: '100%' }}>
+          {/* <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection /> */}
+        </div>
+      </div>
   );
 }
 
