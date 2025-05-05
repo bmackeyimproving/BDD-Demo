bbdbbab8e3f425789170cfdeec42397e5f0d8b37
bmackeyimproving <brian.mackey@improving.com>
2025-05-04 19:38:56 -0500

    Add Parent Component architecture example

--- bdd-vite-react/src/pages/PageThree.jsx
+++ bdd-vite-react/src/pages/PageThree.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useSharedService } from '../services/sharedService';
+import Child from '../Components/Child';
+import { useState } from 'react';
 
 function PageThree() {
   const { getMessage } = useSharedService();
+  const [parentMessage] = useState('Hello from parent!');
 
   return (
-      <div>
-        <h1>Page Three</h1>
-        <p>{getMessage()}</p>
-      </div>
+    <div>
+      <h1>Page Three</h1>
+      <p>{getMessage()}</p>
+      <p>{parentMessage}</p>
+      <Child message={parentMessage} />
+    </div>
   );
 }
 
