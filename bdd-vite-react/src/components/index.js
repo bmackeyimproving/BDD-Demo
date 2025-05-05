90b162adafc93f9a8259d8d57736d10f1f96889b
bmackeyimproving <brian.mackey@improving.com>
2025-05-04 20:00:25 -0500

    Update index.js

--- bdd-vite-react/src/components/index.js
+++ bdd-vite-react/src/components/index.js
@@ -1,2 +1,5 @@
 export { default as DataGridWrapper } from './DataGridWrapper';
 export { default as Child } from './Child';
+export { default as HelloWorld } from './HelloWorld';
+export { default as Greeting } from './Greeting';
+export { default as Adder } from './Adder';
